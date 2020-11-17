002a90ea50d107419f5eba978bc5198a3514e243
lucycchi <lucycchi@gmail.com>
2020-11-16 18:58:42 -0800

    new

--- src/data/channels.js
+++ src/data/channels.js
@@ -6,6 +6,5 @@ export async function getChannelById(id) {
   let cached = cachedChannelRecords[id];
   if (typeof cached !== 'undefined') return await cached;
   cached = cachedChannelRecords[id] = apiCall(`Channels/${id}`);
-
   return await cached;
 }
